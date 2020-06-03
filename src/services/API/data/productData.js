c14dd05c71badf849c015d6899d6776ad4837a04
mauriciodesign <mauricio.a.design@gmail.com>
2020-06-03 16:49:57 -0400

    my_store

---
+++ src/services/API/data/productData.js
@@ -0,0 +1,18 @@
+export default {
+  data: [
+    { name: 'Casa', id: '0', price: 100.0, img: 'product_house.jpg' },
+    { name: 'Bote', id: '1', price: 100.0, img: 'product_boat.jpg' },
+    { name: 'Avión', id: '2', price: 100.0, img: 'product_airplane.jpg' },
+    { name: 'Motocicleta', id: '3', price: 100.0, img: 'product_motorbike.jpg' },
+    { name: 'Computadora', id: '4', price: 100.0, img: 'product_computer.jpg' },
+    { name: 'Silla', id: '5', price: 100.0, img: 'product_chair.jpg' },
+    { name: 'Espejo', id: '6', price: 100.0, img: 'product_mirror.jpg' },
+    { name: 'Mesa', id: '7', price: 100.0, img: 'product_table.jpg' },
+    { name: 'Café', id: '8', price: 100.0, img: 'product_coffee.jpg' },
+    { name: 'Bicicleta', id: '9', price: 100.0, img: 'product_bicycle.jpg' },
+    { name: 'Gato', id: '10', price: 100.0, img: 'product_cat.jpg' },
+    { name: 'Parlante', id: '11', price: 100.0, img: 'product_speaker.jpg' },
+    { name: 'Plancha', id: '12', price: 100.0, img: 'product_laundry.jpg' },
+    { name: 'Libro', id: '13', price: 100.0, img: 'product_book.jpg' },
+  ]
+}
